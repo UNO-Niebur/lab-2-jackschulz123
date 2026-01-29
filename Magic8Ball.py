6a9b976252aedcdb15088f420edccff6a6a17ec9
jackschulz123 <jackschulz2025@gmail.com>
2026-01-29 22:27:29 +0000

    finished 8 ball

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,5 +1,5 @@
 #Magic8Ball.py
-#Name:
+#Name: Jack Schulz
 #Date:
 #Assignment:
 
@@ -10,9 +10,20 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
+  question = input("What is your yes or no question?: ")
+  answers = ["As I see it, yes.", "Ask again later", "Better not tell you now.", "Cannot predict now.",
+              "Concentrate and ask again.", "Don't count on it", "It is certain.", "It is decidedly so.",
+               "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.",
+               "Outlook good.", "Reply hazy, try again", "signs point to yes", "very doubtful",
+                "without a doubt", "yes.", "yes - definitely", "You may rely on it."]
 
   #Answer question randomly with one of the options from your earlier list.
-
+  length = len(answers) 
+  r = random.random() * length
+  r = int(r)
+  response = answers[r]
+  print(response)
+ 
 
 if __name__ == '__main__':
   main()
